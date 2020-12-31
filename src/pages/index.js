c6276532e2611e718f7461c49a79c2ad65d74801
willjw3 <will.j.ward3@gmail.com>
2020-12-31 12:37:07 +0900

    removed .env

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,34 +2,25 @@ import React, {useState, useEffect} from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "../styles/index.css"
-import fetch from "node-fetch"
 
 const Index = () => {
-    const [beers, setBeers] = useState([])
 
-    useEffect(() => {
-        const getBeerList = async () => {
-            const response = await fetch("/.netlify/functions/getbeerlist")
-            const data = await response.json()
-            console.log(data)
-            setBeers(data)
-        }
-        getBeerList()
-    }, [])
-    console.log(beers.beers && beers.beers)
     return (
         <div className="index">
             <Header />
             <div className="main">
                 <h1>The List</h1>
                 <div>
-                    {
-                        beers.beers && beers.beers.map((beer, i) => {
-                            return (
-                                <p key={i}>{beer.beer}</p>
-                            )
-                        })
-                    }
+                   <p>List Item 1</p>
+                   <p>List Item 2</p>
+                   <p>List Item 3</p>
+                   <p>List Item 4</p>
+                   <p>List Item 5</p>
+                   <p>List Item 6</p>
+                   <p>List Item 7</p>
+                   <p>List Item 8</p>
+                   <p>List Item 9</p>
+                   <p>List Item 10</p>
                 </div>
             </div>
             <Footer />
